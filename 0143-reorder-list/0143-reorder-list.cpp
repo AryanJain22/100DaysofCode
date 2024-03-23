22cdb553eaef8828d4eed31028a3f427c73f8805
Aryan Jain <121332370+AryanJain22@users.noreply.github.com>
2024-03-23 21:06:02 +0530

    Time: 26 ms (46.48%), Space: 21.3 MB (69.02%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode *head) {
+        if (head == NULL
+        || head->next == NULL
+        || head->next->next == NULL) {
+            return;
+        }
+        
+        ListNode *mid = head;
+        ListNode *end = head;
+        while (end->next != NULL) {
+            mid = mid->next;
+            end = end->next;
+            if (end->next == NULL) {
+                break;
+            } else {
+                end = end->next;
+            }
+        }
+        
+        ListNode *current = mid->next;
+        ListNode *prev = mid;
+        mid->next = NULL;
+        while (current != NULL) {
+            ListNode *tmp = current->next;
+            current->next = prev;
+            prev = current;
+            current = tmp;
+        } // upon termination, current == NULL and:
+
+        current = head;
+        ListNode *leftCurrent = head->next;
+        ListNode *rightCurrent = prev;
+        bool isLeft = false;
+        while (leftCurrent != mid && rightCurrent != NULL) {
+            if (isLeft) {
+                current->next = leftCurrent;
+                leftCurrent = leftCurrent->next;
+            } else {
+                current->next = rightCurrent;
+                rightCurrent = rightCurrent->next;
+            }
+            current = current->next;
+            isLeft = !isLeft;
+        }
+        if (leftCurrent != mid) {
+            current->next = leftCurrent;
+            while (leftCurrent != mid) {
+                prev = leftCurrent;
+                leftCurrent = leftCurrent->next;
+            }
+            prev->next = NULL;
+        }
+        if (rightCurrent != NULL) {
+            current->next = rightCurrent;
+        }
+    }
+};
