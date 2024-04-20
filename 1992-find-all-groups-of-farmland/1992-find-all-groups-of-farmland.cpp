861d6ead7b185c01396ca3fa908e30bfa79b71fc
Aryan Jain <121332370+AryanJain22@users.noreply.github.com>
2024-04-21 01:38:57 +0530

    Time: 80 ms (98.71%), Space: 70.6 MB (61.49%) - LeetHub

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& land, int i, int j, int& r2, int& c2) {
+        if (i < 0 || i >= land.size() || j < 0 || j >= land[0].size() ||
+            land[i][j] != 1) {
+            return;
+        }
+        land[i][j] = 0;
+        r2 = max(r2, i);
+        c2 = max(c2, j);
+        dfs(land, i + 1, j, r2, c2);
+        dfs(land, i - 1, j, r2, c2);
+        dfs(land, i, j + 1, r2, c2);
+        dfs(land, i, j - 1, r2, c2);
+    }
+    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        vector<vector<int>> result;
+        int m = land.size();
+        int n = land[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (land[i][j] == 1) {
+                    int r1 = i, c1 = j, r2 = i, c2 = j;
+                    dfs(land, i, j, r2, c2);
+                    result.push_back({r1, c1, r2, c2});
+                }
+            }
+        }
+
+        return result;
+    }
+};
